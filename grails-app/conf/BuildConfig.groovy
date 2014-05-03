ca330067d29714f14910701ce4bba1bb8634787d
Shan <shansrinivasan@yahoo.com>
2014-05-03 14:55:05 -0400

    heroku changes

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.0.3"
         compile ':cache:1.1.2'
-
+		runtime 'postgresql:postgresql:8.4-702.jdbc3'
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.13" // or ":hibernate4:4.3.5.1"
         runtime ":database-migration:1.4.0"
